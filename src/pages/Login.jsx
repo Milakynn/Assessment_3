dda38707aba8888f9206694cd4cdce9388be1f29
Milakynn <lindyrella.k@gmail.com>
2024-11-30 08:02:10 +0000

    Validation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,32 +3,47 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import BreadcrumbItem from 'react-bootstrap/BreadcrumbItem'
 import { Link } from 'react-router-dom'
 
-export function Login( props ) {
+export function Login(props) {
     document.title = "Login ?"
     return (
         <Container>
             <Breadcrumb className="mt-2">
-                <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/"}}>Home</Breadcrumb.Item>
+                <Breadcrumb.Item linkAs={Link} linkProps={{ to: "/" }}>Home</Breadcrumb.Item>
                 <Breadcrumb.Item active>Log in</Breadcrumb.Item>
             </Breadcrumb>
             <Row>
-            <h2 className="mt-4">Sign in</h2>
-                <Col md = {{ span: 4, offset: 4}}>
+                <h2 className="mt-4">Sign in</h2>
+                <Col md={{ span: 7, offset: 2 }}>
                     <Form>
                         <Form.Group className="mt-2">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control name="email" type="email" placeholder="user@gmail.com" required/>
+                            <Form.Label>Username</Form.Label>
+                            <InputGroup hasValidation>
+                                <InputGroup.Text id="inputGroupPretend">@</InputGroup.Text>
+                                <Form.Control name="User" type="text" placeholder="Lindyrella11" aria-describedby="inputGroupPretend" required />
+                                <Form.Control.Feedback type="invalid">Please choose a username.</Form.Control.Feedback>
+                            </InputGroup>
                         </Form.Group>
-                        <Form.Group className="mt-2">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control name="password" type="password" placeholder="minimum 8 characters" required/>
-                        </Form.Group>
+                        <Row>
+                            <Col md={6}>
+                                <Form.Group className="mt-2">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control name="email" type="email" placeholder="user@gmail.com" required />
+                                </Form.Group>
+                            </Col>
+                            <Col md={6}>
+                                <Form.Group className="mt-2">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control name="password" type="password" placeholder="minimum 8 characters" required />
+                                </Form.Group>
+                            </Col>
+                        </Row>
                         <Form.Group className="mt-2 mb-1" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
+                            <Form.Check type="checkbox" id="autoSizingCheck" label="Remember me" />
                         </Form.Group>
                         <Button className="mt-3" variant="outline-info" type="submit">
                             Sing up
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,27 +1,28 @@
-import { Container, InputGroup } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-// import InputGroup from 'react-bootstrap/InputGroup'
+import InputGroup from 'react-bootstrap/InputGroup'
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import BreadcrumbItem from 'react-bootstrap/BreadcrumbItem'
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 export function Register(props) {
     document.title = "Sign up to go"
 
-    // const [validated, setValidated] = useState(false);
+    const [validated, setValidated] = useState(false);
 
-    // const handleSubmit = (event) => {
-    //     const form = event.currentTarget;
-    //     if (form.checkValidity() === false) {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-    //     }
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
 
-    //     setValidated(true);
-    // };
+        setValidated(true);
+    };
 
 
     return (
@@ -33,31 +34,35 @@ export function Register(props) {
             <Row>
                 <h2 className="mt-4">Sign up</h2>
                 <Col md={{ span: 7, offset: 2 }}>
-                    <Form className="mb-5">
-                        {/* <h2>Sign up</h2> */}
-                        <Form.Group className="mt-2">
+                    <Form className="mb-5" noValidate validated={validated} onSubmit={handleSubmit}>
+                        <Form.Group className="mt-2" controlId="validationCustomUsername">
                             <Form.Label>Username</Form.Label>
                             <InputGroup hasValidation>
                                 <InputGroup.Text id="inputGroupPretend">@</InputGroup.Text>
                                 <Form.Control name="User" type="text" placeholder="Lindyrella11" aria-describedby="inputGroupPretend" required />
                                 <Form.Control.Feedback type="invalid">Please choose a username.</Form.Control.Feedback>
+                                <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                             </InputGroup>
                         </Form.Group>
                         <Row>
                             <Col md={6}>
-                                <Form.Group className="mt-2">
+                                <Form.Group className="mt-2" controlId="validationCustom01">
                                     <Form.Label>First name</Form.Label>
                                     <Form.Control name="first name" type="text" placeholder="Lindsay" required />
+                                    <Form.Control.Feedback type="invalid">Please enter your first name.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
-                                <Form.Group className="mt-2">
+                                <Form.Group className="mt-2" controlId="validationCustom02">
                                     <Form.Label>Last name</Form.Label>
                                     <Form.Control name="Last name" type="text" placeholder="Kuilagi" required />
+                                    <Form.Control.Feedback type="invalid">Please enter your last name.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                                 </Form.Group>
                             </Col>
                         </Row>
-                        <Form.Label className="mt-2">Gender</Form.Label>
+                        <Form.Label className="mt-2" controlId="validationCustomGender">Gender</Form.Label>
                         <Form.Select aria-label="Default select example" required>
                             <option>-</option>
                             <option value="1">Male</option>
@@ -65,51 +70,67 @@ export function Register(props) {
                             <option value="3">Non-binary</option>
                             <option value="4">Prefer not to say</option>
                         </Form.Select>
-                        <Form.Group className="mt-2">
+                        <Form.Control.Feedback type="invalid">Please select an option.</Form.Control.Feedback>
+                        <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
+                        <Form.Group className="mt-2" controlId="validationCustomEmail">
                             <Form.Label>Email address</Form.Label>
                             <Form.Control name="email" type="email" placeholder="user@gmail.com" required />
+                            <Form.Control.Feedback type="invalid">Please provide an email.</Form.Control.Feedback>
+                            <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                         </Form.Group>
                         <Row className="align-middle">
                             <Col md={6}>
-                                <Form.Group className="mt-2">
+                                <Form.Group className="mt-2" controlId="validationCustomPassword01">
                                     <Form.Label>Password</Form.Label>
                                     <Form.Control name="password" type="password" placeholder="minimum 8 characters" required />
+                                    <Form.Control.Feedback type="invalid">Please provide a password.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
-                                <Form.Group className="mt-2">
+                                <Form.Group className="mt-2" controlId="validationCustomPassword02">
                                     <Form.Label>Confirm password</Form.Label>
                                     <Form.Control name="password" type="password" placeholder="minimum 8 characters" required />
+                                    <Form.Control.Feedback type="invalid">Please confirm password.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                                 </Form.Group>
                             </Col>
                         </Row>
-                        <Form.Group className="mt-2">
+                        <Form.Group className="mt-2" controlId="validationCustom03">
                             <Form.Label>Phone number</Form.Label>
                             <Form.Control name="phone number" type="text" placeholder="0420-455-259" required />
+                            <Form.Control.Feedback type="invalid">Please provide a valid phone number.</Form.Control.Feedback>
+                            <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                         </Form.Group>
                         <Row className="align-middle">
                             <Col md={6}>
-                                <Form.Group className="mt-2">
+                                <Form.Group className="mt-2" controlId="validationCustom04">
                                     <Form.Label>Address</Form.Label>
                                     <Form.Control name="address" type="text" placeholder="18 Albert St" required />
+                                    <Form.Control.Feedback type="invalid">Please provide a valid address.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
-                                <Form.Group className="mt-2">
+                                <Form.Group className="mt-2" controlId="validationCustom05">
                                     <Form.Label>Suburb</Form.Label>
                                     <Form.Control name="suburb" type="text" placeholder="Chastwood" required />
+                                    <Form.Control.Feedback type="invalid">Please provide a valid suburb.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                                 </Form.Group>
                             </Col>
                         </Row>
                         <Row className="align-middle">
                             <Col md={6}>
-                                <Form.Group className="mt-2">
+                                <Form.Group className="mt-2" controlId="validationCustom06">
                                     <Form.Label>Post code</Form.Label>
                                     <Form.Control name="post code" type="text" placeholder="2193" required />
+                                    <Form.Control.Feedback type="invalid">Please provide a valid post code.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                                 </Form.Group>
                             </Col>
                             <Col md={6}>
-                                <Form.Label className="mt-2">State</Form.Label>
+                                <Form.Label className="mt-2" controlId="validationCustomState">State</Form.Label>
                                 <Form.Select aria-label="Default select example" required>
                                     <option>Select a state</option>
                                     <option value="1">NT</option>
@@ -121,6 +142,8 @@ export function Register(props) {
                                     <option value="7">WA</option>
                                     <option value="8">TAS</option>
                                 </Form.Select>
+                                <Form.Control.Feedback type="invalid">Please select an option.</Form.Control.Feedback>
+                                <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
                             </Col>
                         </Row>
                         <Form.Group className="mt-2 mb-1" controlId="formBasicCheckbox">
