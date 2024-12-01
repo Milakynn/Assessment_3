c30adc15447c495f9ae179fdda34e05e72e25695
Milakynn <lindyrella.k@gmail.com>
2024-12-01 03:09:44 +0000

    Contact pofinage

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -33,38 +33,50 @@ export function Contact(props) {
                 <Col md={{ span: 6, offset: 3 }}>
                     <h2>Email us</h2>
                     <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                        <Form.Group className="mt-2" controlId="validationCustomEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control name="email" type="email" placeholder="user@gmail.com" required />
-                            <Form.Control.Feedback type="invalid">Please provide an email.</Form.Control.Feedback>
-                            <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label className="mt-2" controlId="validationCustomEnquiry">Enquiry type</Form.Label>
-                            <Form.Select aria-label="Default select example" required>
-                                <option>-</option>
-                                <option value="1">Feedback</option>
-                                <option value="2">Stores</option>
-                                <option value="3">Loyalty/Giftcard</option>
-                                <option value="4">Career/HR</option>
-                                <option value="5">Catering</option>
-                            </Form.Select>
-                            <Form.Control.Feedback type="invalid">Please select an option.</Form.Control.Feedback>
-                            <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
-                            <Form.Text>What is your email related to?</Form.Text>
-                        </Form.Group>
-                        <Form.Group className="mt-2" controlId="validationCustom01">
-                            <Form.Label>Full name</Form.Label>
-                            <Form.Control name="full name" type="text" placeholder="Lindsay Kuilagi" required />
-                            <Form.Control.Feedback type="invalid">Please provide your full name.</Form.Control.Feedback>
-                            <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
-                        </Form.Group>
-                        <Form.Group className="mt-2" controlId="validationCustom02">
-                            <Form.Label>Phone number</Form.Label>
-                            <Form.Control name="phone number" type="text" placeholder="0421-488-547" required />
-                            <Form.Control.Feedback type="invalid">Please provide a valid phone number.</Form.Control.Feedback>
-                            <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
-                        </Form.Group>
+                        <Row>
+                            <Col md={6}>
+                                <Form.Group className="mt-2" controlId="validationCustomEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control name="email" type="email" placeholder="user@gmail.com" required />
+                                    <Form.Control.Feedback type="invalid">Please provide an email.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
+                                </Form.Group>
+                            </Col>
+                            <Col md={6}>
+                                <Form.Group>
+                                    <Form.Label className="mt-2" controlId="validationCustomEnquiry">Enquiry type</Form.Label>
+                                    <Form.Select aria-label="Default select example" required>
+                                        <option>-</option>
+                                        <option value="1">Feedback</option>
+                                        <option value="2">Stores</option>
+                                        <option value="3">Loyalty/Giftcard</option>
+                                        <option value="4">Career/HR</option>
+                                        <option value="5">Catering</option>
+                                    </Form.Select>
+                                    <Form.Control.Feedback type="invalid">Please select an option.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
+                                    <Form.Text>What is your email related to?</Form.Text>
+                                </Form.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col md={6}>
+                                <Form.Group className="mt-2" controlId="validationCustom01">
+                                    <Form.Label>Full name</Form.Label>
+                                    <Form.Control name="full name" type="text" placeholder="Lindsay Kuilagi" required />
+                                    <Form.Control.Feedback type="invalid">Please provide your full name.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
+                                </Form.Group>
+                            </Col>
+                            <Col md={6}>
+                                <Form.Group className="mt-2" controlId="validationCustom02">
+                                    <Form.Label>Phone number</Form.Label>
+                                    <Form.Control name="phone number" type="text" placeholder="0421-488-547" required />
+                                    <Form.Control.Feedback type="invalid">Please provide a valid phone number.</Form.Control.Feedback>
+                                    <Form.Control.Feedback type="valid">Looks good!</Form.Control.Feedback>
+                                </Form.Group>
+                            </Col>
+                        </Row>
                         <Form.Group className="mt-2">
                             <Form.Label>Leave us a message</Form.Label>
                             <Form.Control name="Message" as="textarea" placeholder="Leave your comment here..." style={{ height: '160px' }} required />
